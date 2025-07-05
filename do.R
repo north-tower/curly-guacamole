ab3edbafb24ae4315e8a54340329928afea5e762
north-tower <muneshmyke@gmail.com>
2025-07-05 12:23:42 +0300

    hjj

--- do.R
+++ do.R
@@ -15,9 +15,6 @@ library(lubridate)  # Date handling (from tidyverse)
 library(broom)
 
 
-# Set the file location of the R scripts
-setwd("~/Website Code files/Web_SR_Analysis")
-
 # Load and run each R script
 source("load.R")
 source("clean.R")
