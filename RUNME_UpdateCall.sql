295f99df845574e0f162847a97df82d57d314730
north-tower <muneshmyke@gmail.com>
2025-07-05 15:00:45 +0300

    kkjj

--- RUNME_UpdateCall.sql
+++ RUNME_UpdateCall.sql
@@ -1,15 +1,14 @@
 DELIMITER $$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `RUNME_UpdateCall`()
+CREATE DEFINER=`smartform_user`@`localhost` PROCEDURE `RUNME_UpdateCall`()
 BEGIN
-CALL `smartform`.`SR_sample_races_UPDATE`();
-CALL `smartform`.`SR_data_UPDATE`();
-CALL `smartform`.`SR_daily_data_UPDATE`();
-CALL `smartform`.`my_daily_races_UPDATE`();
-CALL `smartform`.`adv_my_daily_races_UPDATE`();
+CALL `coolwed1_WP9PN`.`SR_sample_races_UPDATE`();
+CALL `coolwed1_WP9PN`.`SR_data_UPDATE`();
+CALL `coolwed1_WP9PN`.`my_daily_races_UPDATE`();
+CALL `coolwed1_WP9PN`.`adv_my_daily_races_UPDATE`();
 
-CALL `smartform`.`adv_my_daily_details_tb_UPDATE`();
-CALL `smartform`.`my_daily_details_tb_UPDATE`();
+CALL `coolwed1_WP9PN`.`adv_my_daily_details_tb_UPDATE`();
+CALL `coolwed1_WP9PN`.`my_daily_details_tb_UPDATE`();
 
 END$$
 
