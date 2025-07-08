1f28f88e8506df3a854d5e7cbd89ee18cf67a7d0
north-tower <muneshmyke@gmail.com>
2025-07-08 21:12:20 +0300

    jjj

--- write.R
+++ write.R
@@ -14,12 +14,15 @@ smartformDB <- dbConnect(RMariaDB::MariaDB(),
 ## Query for Loading and performing any further data/table formatting
 #######################################
 
+print(paste("Writing", nrow(SR_data), "rows to sr_results"))
 dbExecute(smartformDB, "DROP TABLE IF EXISTS sr_results")
 dbWriteTable(smartformDB, "sr_results", SR_data)
 
+print(paste("Writing", nrow(draw_bias), "rows to draw_bias"))
 dbExecute(smartformDB, "DROP TABLE IF EXISTS draw_bias")
 dbWriteTable(smartformDB, "draw_bias", draw_bias)
 
+print("Calling adv_speed_analysis_UPDATE and speed_analysis_UPDATE procedures")
 dbExecute(smartformDB, "CALL `coolwed1_WP9PN`.`adv_speed_analysis_UPDATE`()")
 dbExecute(smartformDB, "CALL `coolwed1_WP9PN`.`speed_analysis_UPDATE`()")
 
