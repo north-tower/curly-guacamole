16b9029a8664ff1c35190ada2e66e8139a773704
north-tower <muneshmyke@gmail.com>
2025-07-18 10:09:25 +0300

    hh

--- RUNME_UpdateCall.sql
+++ RUNME_UpdateCall.sql
@@ -1,27 +1,27 @@
 DELIMITER $$
 CREATE PROCEDURE `RUNME_UpdateCall` ()
 BEGIN
-CALL `smartform`.`SR_sample_races_UPDATE`();
-CALL `smartform`.`SR_data_UPDATE`();
-CALL `smartform`.`separated_comments_UPDATE`();
-CALL `smartform`.`todays_runners_insights_UPDATE`();
-CALL `smartform`.`ancestry_records_UPDATE`();
+CALL `coolwed1_WP9PN`.`SR_sample_races_UPDATE`();
+CALL `coolwed1_WP9PN`.`SR_data_UPDATE`();
+CALL `coolwed1_WP9PN`.`separated_comments_UPDATE`();
+CALL `coolwed1_WP9PN`.`todays_runners_insights_UPDATE`();
+CALL `coolwed1_WP9PN`.`ancestry_records_UPDATE`();
 
-CALL `smartform`.`separated_comment_count_UPDATE`();
-CALL `smartform`.`my_daily_races_UPDATE`();
-CALL `smartform`.`adv_my_daily_races_UPDATE`();
+CALL `coolwed1_WP9PN`.`separated_comment_count_UPDATE`();
+CALL `coolwed1_WP9PN`.`my_daily_races_UPDATE`();
+CALL `coolwed1_WP9PN`.`adv_my_daily_races_UPDATE`();
 
-CALL `smartform`.`14DayTrainerStats_UPDATE`();
-CALL `smartform`.`14DayJockeyStats_UPDATE`();
-CALL `smartform`.`separated_comments_win_value_UPDATE`();
-CALL `smartform`.`adv_my_daily_details_tb_UPDATE`();
-CALL `smartform`.`my_daily_details_tb_UPDATE`();
+CALL `coolwed1_WP9PN`.`14DayTrainerStats_UPDATE`();
+CALL `coolwed1_WP9PN`.`14DayJockeyStats_UPDATE`();
+CALL `coolwed1_WP9PN`.`separated_comments_win_value_UPDATE`();
+CALL `coolwed1_WP9PN`.`adv_my_daily_details_tb_UPDATE`();
+CALL `coolwed1_WP9PN`.`my_daily_details_tb_UPDATE`();
 
-CALL `smartform`.`DailyRacecard14_UPDATE`();
+CALL `coolwed1_WP9PN`.`DailyRacecard14_UPDATE`();
 
-CALL `smartform`.`daily_comment_ratings_UPDATE`();
-CALL `smartform`.`SR_daily_data_UPDATE`();
+CALL `coolwed1_WP9PN`.`daily_comment_ratings_UPDATE`();
+CALL `coolwed1_WP9PN`.`SR_daily_data_UPDATE`();
 
-CALL `smartform`.`daily_race_comment_history_UPDATE`();
+CALL `coolwed1_WP9PN`.`daily_race_comment_history_UPDATE`();
 END$$
 DELIMITER ;
